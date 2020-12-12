baec4c95c37b1ad8cc129eef510e6a2d6f783756
Nguyen Thi Tien <tien.nguyen21@pn.com.vn>
2020-12-12 08:10:52 +0700

    login

--- app/src/main/java/com/example/login2/MainActivity.java
+++ app/src/main/java/com/example/login2/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.login2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button =  findViewById(R.id.ok);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity2.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+    }
+
+//    EditText email = (EditText) findViewById(R.id.email);
+//    EditText password = (EditText) findViewById(R.id.pw);
+
+    public void login(View view) {
+//        if (email.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+            Intent intent = new Intent(getApplicationContext(),AppCompatActivity.class);
+            startActivity(intent);
+//        }
     }
 }
